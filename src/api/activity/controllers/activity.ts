40db5165f55ee40a46fcf3828239d791228bb9df
TechQuery <shiy2008@gmail.com>
2025-06-18 05:52:42 +0800

    [optimize] update Partnership & Activity models
    Signed-off-by: TechQuery <shiy2008@gmail.com>

---
+++ src/api/activity/controllers/activity.ts
@@ -0,0 +1,7 @@
+/**
+ * activity controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::activity.activity');
---
+++ src/api/partnership/controllers/partnership.ts
@@ -0,0 +1,7 @@
+/**
+ * partnership controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::partnership.partnership');
