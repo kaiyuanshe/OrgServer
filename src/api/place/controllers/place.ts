708b0e1c08a0387851c6ef41b302dc0c3afeedfa
TechQuery <shiy2008@gmail.com>
2025-06-19 07:11:40 +0800

    [optimize] update Period & Place models
    Signed-off-by: TechQuery <shiy2008@gmail.com>

---
+++ src/api/place/controllers/place.ts
@@ -0,0 +1,7 @@
+/**
+ * place controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::place.place');
---
+++ src/api/place/routes/place.ts
@@ -0,0 +1,7 @@
+/**
+ * place router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::place.place');
