bc29a253c5f33776a71e59c6dce12d9c1c8b8691
TechQuery <shiy2008@gmail.com>
2025-06-19 22:04:52 +0800

    [optimize] update Program, Account & Evaluation models
    Signed-off-by: TechQuery <shiy2008@gmail.com>

---
+++ src/api/account/controllers/account.ts
@@ -0,0 +1,7 @@
+/**
+ * account controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::account.account');
---
+++ src/api/program/routes/program.ts
@@ -0,0 +1,7 @@
+/**
+ * program router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::program.program');
